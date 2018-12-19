88a4b94c29e7fa3af34a38c559f241f1a86e94fe
Dinesh-Desktop <ssaini@meritech.co.in>
2018-12-19 17:59:06 +0530

    -Added Firebase Function in only Category 2

--- app/src/main/java/com/example/dineshkumar/diary/AddCategory.java
+++ app/src/main/java/com/example/dineshkumar/diary/AddCategory.java
@@ -1,11 +1,10 @@
 package com.example.dineshkumar.diary;
 
 import android.content.DialogInterface;
-import android.os.Debug;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -24,9 +23,11 @@ import butterknife.OnClick;
 
 public class AddCategory extends AppCompatActivity {
     public static final String TAG = "com.example.dineshkumar.diary.AddCategory";
-    public static final String FIREBASE_CATEG_REFERENCE ="Category";
+    public static final String FIREBASE_CATEG_REFERENCE = "Category";
+    public static final String FIREBASE_ROOT_REFERENCE = "Root";
     @BindView(R.id.catgName)
     EditText edCatgName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +38,7 @@ public class AddCategory extends AppCompatActivity {
     }
 
     @OnClick(R.id.addCatgBtn)
-    void addCategory()
-    {
+    void addCategory() {
         Category category = new Category(edCatgName.getText().toString());
         CategoryDatabase database = new CategoryDatabase(this);
         database.addCategory(category.getName());
@@ -49,31 +49,30 @@ public class AddCategory extends AppCompatActivity {
 
     private void addToFirebase(final Category category) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference(FIREBASE_CATEG_REFERENCE);
+        final DatabaseReference myRef = database.getReference(FIREBASE_ROOT_REFERENCE);
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (!dataSnapshot.child(category.getName()).exists()) {
-                    myRef.setValue(category);
-
-                    Log.i("myTag","Category added");
+                    String email = "blahblah";
+                    myRef.child(email).child(FIREBASE_CATEG_REFERENCE).setValue(category);
+                    Log.i("myTag", "Category added");
                 } else {
                     Toast.makeText(getBaseContext(), "Oh ! unable to Update !!", Toast.LENGTH_SHORT).show();
-                    Log.i("myTag","Problem while adding ");
+                    Log.i("myTag", "Problem while adding ");
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.i("myTag","Problem while adding ");
+                Log.i("myTag", "Problem while adding ");
                 Toast.makeText(getBaseContext(), "ErrorTag :" + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
-    void showCloseConfirmDialog()
-    {
+    void showCloseConfirmDialog() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 // Add the buttons
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -84,7 +83,7 @@ public class AddCategory extends AppCompatActivity {
         builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 Toast.makeText(AddCategory.this, "Category added successfully !", Toast.LENGTH_SHORT).show();
-           finish();
+                finish();
             }
         });
 
@@ -92,10 +91,11 @@ public class AddCategory extends AppCompatActivity {
         dialog.setTitle("Want to add more category ?");
         dialog.show();
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
-        Log.i("backTg","Back pressed");
+        Log.i("backTg", "Back pressed");
         return true;
     }
 }
